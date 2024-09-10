40750a3f6473509cb9c2aebd583b1f0c6c9a0b4c
Simon Halland <simon.halland@knowit.no>
2024-09-10 15:12:27 +0200

    lower case url redirect

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,22 @@ import './styles/global.css';  // Import global styles
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Check if URL contains uppercase letters and redirect to lowercase URL
+const currentUrl = window.location.href;
+const lowercaseUrl = currentUrl.toLowerCase();
+
+if (currentUrl !== lowercaseUrl) {
+  window.location.href = lowercaseUrl;
+} else {
+  const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+  );
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
